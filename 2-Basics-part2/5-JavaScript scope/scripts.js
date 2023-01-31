2d19c7d53c1c5b221900251a51a460cc51791633
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-01-31 15:17:36 +0530

    JavaScript Scope - completed

---
+++ 2-Basics-part2/5-JavaScript scope/scripts.js
@@ -0,0 +1,10 @@
+// Scope determines the accessibility (visibility) of variables. In JavaScript there are two types of scope.
+//     1. Local
+//     2. Global
+
+// --Local Variables--
+// Variables declared within a JavaScript function, They can only be accessed from within the function. Local variables are deleted when the function is completed.
+
+
+// --Global Variables--
+// A variable declared outside a function, All scripts and functions on a web page can access it. In a web browser, global variables are deleted when you close the browser window (or tab)
