4af0ee8df3f59ccad303786b2533ea6d9f4e1590
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-03-01 10:27:45 +0530

    promise

---
+++ 4-Modern JS/promise, async, await.js
@@ -0,0 +1,22 @@
+console.log("Start");
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("Reject");
+  }, 0000);
+});
+promise
+  .then((sucsess) => {
+    console.log(sucsess);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+console.log("End");
+
+// console.log(promise);
+
+//Promise (object)==> function with 2 parameters
+//then(property of a promise object - collect the sucsess scenario) => function with 1 parameter
+//catch(property of a promise object - collect the error scenario) => function with 1 parameter
