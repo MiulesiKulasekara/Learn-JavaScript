c0ee200d3e00ab0e908cc6ae1ec13daef7cc8fa9
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-03-01 13:27:53 +0530

    assync, await

--- 4-Modern JS/promise, async, await.js
+++ 4-Modern JS/promise, async, await.js
@@ -1,22 +1,43 @@
 console.log("Start");
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject("Reject");
-  }, 3000);
-});
-promise
-  .then((sucsess) => {
-    console.log(sucsess);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// ----------Promise----------
 
-console.log("End");
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("Reject");
+//   }, 3000);
+// });
+// promise
+//   .then((sucsess) => {
+//     console.log(sucsess);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log("End");
 
 // console.log(promise);
 
+// ------------------------------
+
+// ----------async, await--------
+console.log("Start");
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Resolve");
+  }, 3000);
+});
+
+function doSomething() {
+  const res = promise;
+  console.log(res);
+}
+
+doSomething();
+
+console.log("end");
 //Promise (object)==> function with 2 parameters
 //then(property of a promise object - collect the sucsess scenario) => function with 1 parameter
 //catch(property of a promise object - collect the error scenario) => function with 1 parameter
