3296868ef18472f6095862dac45a2d870bce5e9d
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-03-01 09:07:50 +0530

    Callbacks - two callback functions

--- 4-Modern JS/callbacks.js
+++ 4-Modern JS/callbacks.js
@@ -8,14 +8,24 @@
 
 console.log("Init");
 
-function myFunction(number, callback) {
-  console.log(`The number is ${number}`);
-  callback();
+function myFunction(number, suc, err) {
+  if (number % 2 == 0) {
+    suc();
+  } else {
+    err();
+  }
 }
 
-myFunction(10, () => {
-  console.log(`Hello. I am a callback function!`);
-});
+myFunction(
+  11,
+  (sucsess) => {
+    console.log("Even number");
+  },
+  (error) => {
+    console.log("Odd number");
+  }
+);
 
 console.log(`End`);
+
 //-------------------------------------------------------------------------------------------------
